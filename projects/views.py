f9878dd30c03c55fee333fdf4e4ac86bd057df91
halima precious <halimaprecious3@gmail.com>
2022-06-12 21:57:05 +0300

    profile view

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
+from .models import *
+from .forms import *
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
+    projects = Projects.get_projects()
 
-    return render(request,'home.html')
+    return render(request,'home.html',{"projects":projects})
+
+
+
+@login_required(login_url='/accounts/login/')
+def user_profiles(request):
+    current_user = request.user
+    owner = current_user
+    projects = Projects.get_by_author(owner)
+    
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.save()
+        return redirect('profile')
+        
+    else:
+        form = ProfileUpdateForm()
+    
+    return render(request, 'registration/profile.html', {"form":form, "projects":projects})
