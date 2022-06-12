48c8d99f175d794225e92ed5af27e3330996bbdb
halima precious <halimaprecious3@gmail.com>
2022-06-12 21:55:43 +0300

    add profile path

--- projects/urls.py
+++ projects/urls.py
@@ -7,6 +7,8 @@ from django.conf.urls.static import static
 urlpatterns =[
     re_path(r'^$',views.home, name= 'home'),
 
+    re_path(r'^accounts/profile/$', views.user_profiles, name='profile'),
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
