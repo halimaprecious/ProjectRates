3afe95af750498950a92212f550cec85f2422a55
halima precious <halimaprecious3@gmail.com>
2022-06-12 15:09:40 +0300

    feat: Projects model

--- projects/models.py
+++ projects/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+
+
+
 # Create your models here.
 
 class Profile(models.Model):
@@ -29,3 +32,46 @@ class Profile(models.Model):
 
         return profile
 
+class Projects(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to ='images/',default='images/proj.png')
+    description = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+    profile =models.ForeignKey(Profile, on_delete= models.CASCADE,null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,blank=True)
+    link = models.URLField()
+    
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def get_projects(cls):
+        projects = cls.objects.all()
+        return projects
+    
+    @classmethod
+    def search_projects(cls, search_term):
+        projects = cls.objects.filter(project_title__icontains=search_term)
+        return projects
+    
+    @classmethod
+    def get_by_author(cls, owner):
+        projects = cls.objects.filter(owner=owner)
+        return projects
+    
+    
+    @classmethod
+    def get_project(request, id):
+        project = Projects.objects.get(pk = id)
+        return project
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-post_date']
+        verbose_name = 'My Project'
+        verbose_name_plural = 'Projects'
