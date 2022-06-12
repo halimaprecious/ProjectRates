ea80949713761e16988fbcf511b717e3f5e34ded
halima precious <halimaprecious3@gmail.com>
2022-06-12 13:09:27 +0300

    feat: database configuration

--- ratings/settings.py
+++ ratings/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'ratings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prjrates',
+        'USER': 'ph',
+    'PASSWORD':'Student11',
     }
 }
 
@@ -109,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
