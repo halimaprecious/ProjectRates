3a74c0f78cbb6c148e19f5d968be777610cd9d64
halima precious <halimaprecious3@gmail.com>
2022-06-12 13:34:07 +0300

    profile model tests

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
+import unittest
+from .models import *
 # Create your tests here.
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(id=1, username='hal', password = 'prex123')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user,User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
