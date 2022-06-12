80b10bb702fad9de7c568adfd8b34829e12a5d84
halima precious <halimaprecious3@gmail.com>
2022-06-12 23:32:44 +0300

    add project upload form

--- projects/forms.py
+++ projects/forms.py
@@ -11,3 +11,13 @@ class ProfileUpdateForm(forms.ModelForm):
         widgets = {
           'bio': forms.Textarea(attrs={'rows':2, 'cols':10,}),
         }
+
+
+class NewProjectForm(forms.ModelForm):
+    class Meta:
+        model = Projects
+        exclude = ['owner', 'post_date', 'profile']
+        widgets = {
+          'description': forms.Textarea(attrs={'rows':4, 'cols':10,}),
+        }
+        
