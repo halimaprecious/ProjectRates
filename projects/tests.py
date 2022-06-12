83604686ccd71d0405d54aceb75fd6e1d6dc5a49
halima precious <halimaprecious3@gmail.com>
2022-06-12 17:09:27 +0300

    tests added

--- projects/tests.py
+++ projects/tests.py
@@ -18,7 +18,35 @@ class TestProfile(TestCase):
     def test_delete_user(self):
         self.user.delete()
 
+class TestProject(TestCase):
 
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='cate')
+        self.project = Projects.objects.create(id=1, title='test post', image='https://ucarecdn.com/0ccf61ff-508e-46c6-b713-db51daa6626e', description='desc',owner=self.user, link='http://ur.coml')
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project, Projects))
+
+    def test_save_project(self):
+        self.project.save_project()
+        post = Projects.objects.all()
+        self.assertTrue(len(post) > 0)
+
+    def test_get_projects(self):
+        self.project.save()
+        posts = Projects.get_projects()
+        self.assertTrue(len(posts) > 0)
+
+    def test_search_project(self):
+        self.project.save()
+        post = Projects.search_projects('test')
+        self.assertTrue(len(post) > 0)
+
+    def test_delete_project(self):
+        self.project.delete_project()
+        post = Projects.search_projects('test')
+        self.assertTrue(len(post) < 1)
 
 if __name__ == '__main__':
     unittest.main()
